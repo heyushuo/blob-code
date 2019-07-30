d26b7f0041bea7b3395ad566750938a7dce514e7
heyushuo <1007561607@qq.com>
2019-07-30 11:52:44 +0800

    test

--- JavaScript/copy.js
+++ JavaScript/copy.js
@@ -194,3 +194,15 @@ function heyu() {
   return result
 }
 console.log(heyu());
+// 用 JavaScript 写一个函数，输入 int 型，返回整数逆序后的字符串。如：输入整型 1234，返回字符串“4321”。
+// 要求必须使用递归函数调用，不能用全局变量，输入函数必须只有一个参数传入，必须返回字符串。
+function test(str) {
+  if (str.length > 1) {
+    var newStr = str.substring(str.length - 1);
+    var olbStr = str.substring(0, str.length - 1);
+    return newStr + test(olbStr)
+  } else {
+    return str
+  }
+}
+console.log(test('12345'));
